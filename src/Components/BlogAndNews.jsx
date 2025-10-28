855f28d71c0fade386bdaf6b63957223a0d7b7d2
Emil L <emillarsson99@gmail.com>
2025-10-28 11:13:53 +0100

    Started on blogAndNews component. Going to start on blog post component

---
+++ src/Components/BlogAndNews.jsx
@@ -0,0 +1,21 @@
+function BlogAndNews() {
+  return (
+    <div className="blog-and-news-container">
+
+        <div className="blog-and-news-headline">
+            <div className="headline">
+                <h3>Latest Blog and News</h3>
+                <h2>Check Out Our Latest Blog and News Update</h2>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+        </div>
+
+        
+            
+        
+
+    </div>
+  )
+}
+
+export default BlogAndNews
