a1bdc24c047579e634cc73bb84f01b17f3b9c4f3
Emil L <emillarsson99@gmail.com>
2025-10-28 10:29:57 +0100

    Tweaked price list components hover effect

--- src/Components/PriceList.jsx
+++ src/Components/PriceList.jsx
@@ -1,9 +1,18 @@
 import Button from "./Button"
+import checkmarkGreen from '/checkmark.svg'
+import checkmarkYellow from '/checkmark-yellow.svg'
+import { useState } from "react"
+
 
 function PriceList({ Text, Price, Unit }) {
+    const [isHovered, setIsHovered] = useState(false)
 
     return(
-        <div className="prices-box">
+        <div 
+            className="prices-box"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <div className="pricing-plan-prices">
                 <div className="unit">{Unit}</div>
                 <div className="price">
@@ -15,23 +24,23 @@ function PriceList({ Text, Price, Unit }) {
                 
             <div className="prices-what-you-get">
                 <div className="what-you-get">
-                    <img src="/checkmark.svg" alt="" />
+                    <img src={isHovered ? checkmarkYellow : checkmarkGreen} alt="" />
                     <p>Nam nec ipsum in dolor</p>
                 </div>
                 <div className="what-you-get">
-                    <img src="/checkmark.svg" alt="" />
+                    <img src={isHovered ? checkmarkYellow : checkmarkGreen} alt="" />
                     <p>Fusce nec ligula ut arcu</p>
                 </div>
                 <div className="what-you-get">
-                    <img src="/checkmark.svg" alt="" />
+                    <img src={isHovered ? checkmarkYellow : checkmarkGreen} alt="" />
                     <p>Aliquam pulvinar arcu in</p>
                 </div>
                 <div className="what-you-get">
-                    <img src="/checkmark.svg" alt="" />
+                    <img src={isHovered ? checkmarkYellow : checkmarkGreen} alt="" />
                     <p>Duis gravida enim porta</p>
                 </div>
                 <div className="what-you-get">
-                    <img src="/checkmark.svg" alt="" />
+                    <img src={isHovered ? checkmarkYellow : checkmarkGreen} alt="" />
                     <p>Etiam eget libero non ligula</p>
                 </div>
                 
