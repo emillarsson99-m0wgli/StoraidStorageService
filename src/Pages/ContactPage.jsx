7ff8ab9e2d7d23cadb6ed58f2dd3ff36560a7628
Emil L <emillarsson99@gmail.com>
2025-10-29 15:54:00 +0100

    Added Title component to contact page, tweaked title component

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import TitleCard from '../Components/TitleCard'
 
 function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <div>
+      <TitleCard Title="Contact Us" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+    </div>
   )
 }
 
