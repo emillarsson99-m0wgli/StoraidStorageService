f4191e6d8905cc9fcfb171734f18ba00b66430ea
Emil L <emillarsson99@gmail.com>
2025-11-03 09:56:25 +0100

    added API, problems with imageUrl, will come back to fix later

--- src/Components/BlogPost.jsx
+++ src/Components/BlogPost.jsx
@@ -2,7 +2,7 @@ import calendar from '/calendar.svg'
 import calendarHovered from '/calendarDark.svg'
 import { useState } from 'react'
 
-function BlogPost() {
+const BlogPost = ({post}) => {
   const [isHovered, setIsHovered] = useState(false)
 
   return (
@@ -10,13 +10,19 @@ function BlogPost() {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-        <div className="blog-img-placeholder">Placeholder</div>
+        <div className="blog-img-placeholder">{post.imageUrl}</div>
         <div className="date">
             <img src={isHovered ? calendarHovered : calendar} alt="" className="calendar-img"/>
-            <p>August 17, 2025</p>
+            <div className="created-text">
+              <p>{post.created}</p>
+            </div>
         </div>
-        <h3>Safe and Secure: The Importance of Choosing the Right Storage</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem…</p>
+        <div className="blog-title">
+          <h3>{post.title}</h3>
+        </div>
+        <div className="blog-description">
+          <p>{post.description}</p>
+        </div>
         <div className="read-more">
         <a href="#">Read More →</a>
         </div>
