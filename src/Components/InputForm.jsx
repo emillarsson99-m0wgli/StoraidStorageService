5e935fc4f2bf36a51702c6db5dc6f221e1bfb4a7
Emil L <emillarsson99@gmail.com>
2025-11-04 15:33:46 +0100

    fixed issue with labelName and inputName

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,18 +1,19 @@
 import Button from "./Button"
 
-function InputForm({ Text, shownText, placeholder, name, labelclassName, inputclassName, inputName, labelName }) {
+function InputForm({ Text, shownText, placeholder, labelclassName, inputclassName, inputName,labelName }) {
   return (
 
     <div className="input-form">
 
       <form action="">
-        <label className={`label ${labelclassName}`}  htmlFor={Text}>{shownText} name={labelName}</label>
+        <label className={`label ${labelclassName}`}  htmlFor={Text} name={`label ${labelName}`}>{shownText} 
         <div className="form-row">
-          <input className={`input ${inputclassName}`} type="text" placeholder={placeholder} name={inputName} />
+          <input className={`input ${inputclassName}`} type="text" placeholder={placeholder} name={`input ${inputName}`} />
           <div>
             <Button Text="Submit" />
           </div>
         </div>
+        </label>
       </form>
 
     </div>
