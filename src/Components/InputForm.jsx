a6e72392467a41a95560c9b77eb42ba915938207
Emil L <emillarsson99@gmail.com>
2025-11-04 15:17:38 +0100

    Changed className for label and input into labelclassName and inputclassName

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,15 +1,17 @@
 import Button from "./Button"
 
-function InputForm({ Text, shownText, placeholder, name, className }) {
+function InputForm({ Text, shownText, placeholder, name, labelclassName, inputclassName }) {
   return (
 
     <div className="input-form">
 
       <form action="">
-        <label className={`label ${className}`}  htmlFor={Text}>{shownText}</label>
-        <input className={`input ${className}`} type="text" placeholder={placeholder} name={name} />
-        <div>
-          <Button Text="Submit" />
+        <label className={`label ${labelclassName}`}  htmlFor={Text}>{shownText}</label>
+        <div className="form-row">
+          <input className={`input ${inputclassName}`} type="text" placeholder={placeholder} name={name} />
+          <div>
+            <Button Text="Submit" />
+          </div>
         </div>
       </form>
 
