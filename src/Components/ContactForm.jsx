a55748328e42f57f0cb799e25f93cc16253acba7
Emil L <emillarsson99@gmail.com>
2025-11-05 14:25:37 +0100

    imported inputFormComponent into contactFormComponent

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
+import InputForm from "./InputForm"
 
 function ContactForm() {
   return (
     <div className="contact-form-container">
         <div className="input-group">
-            <label htmlFor="name" className="contact-label"></label>
-            <input type="text" id="name" className="contact-input" />
+            <InputForm />
         </div>
 
         <div className="input-group">
-            <label htmlFor="email" className="contact-label"></label>
-            <input type="text" id="email" className="contact-input" />
+            <InputForm />
         </div>
 
         <div className="input-group">
-            <label htmlFor="telephone" className="contact-label"></label>
-            <input type="text" id="telephone" className="contact-input" />
+            <InputForm />
         </div>
 
         <div className="input-group">
-            <label htmlFor="subject" className="contact-label"></label>
-            <input type="text" id="subject" className="contact-input" />
+            <InputForm />
         </div>
 
         <div className="input-group">
-            <label htmlFor="comment-questions" className="contact-label"></label>
-            <input type="text" id="comments-questions" className="contact-input" />
+            <InputForm />
         </div>
 
     </div>
