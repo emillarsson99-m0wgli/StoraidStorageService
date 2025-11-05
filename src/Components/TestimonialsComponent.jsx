a51b7b507f8856ba0e80a3cd86db7112d3568272
Emil L <emillarsson99@gmail.com>
2025-11-05 13:56:47 +0100

    Added comments to star rating component and added it to my testimonials

--- src/Components/TestimonialsComponent.jsx
+++ src/Components/TestimonialsComponent.jsx
@@ -1,4 +1,4 @@
-
+import StarRating from "./StarRating"
 
 const TestimonialsComponent = ({item}) => {
 
@@ -6,30 +6,32 @@ const TestimonialsComponent = ({item}) => {
   return (
     <div className="testimonials-box">
         <div className="testimonials-box-content">
+
             <div className="star-rating">
-                <p>{item.rating}/5</p>
-                <img src="/StarIcon.svg" alt="" />
-                {/* <img src="/StarIcon.svg" alt="" />
-                <img src="/StarIcon.svg" alt="" />
-                <img src="/StarIcon.svg" alt="" />
-                <img src="/StarIcon.svg" alt="" /> */}
+                <StarRating rating={item.rating} />
             </div>
+
             <div className="testimonials-comment">
                 <p>{item.comment}</p>
             </div>
             
             <div className="testimonials-person">
+
                 <div className="testimonials-image">
                     <img src={item.avatarUrl} alt="" />
                 </div>
+
                 <div className="testimonials-person-text">
                     <h5>{item.name}</h5>
                     <p>{item.companyName}</p>
                 </div>
+
                 <div className="testimonials-quote">
                     <img src="/quoteIcon.svg" alt="" className="quote-icon" />
                 </div>
+
             </div>
+            
         </div>     
     </div>
   )
