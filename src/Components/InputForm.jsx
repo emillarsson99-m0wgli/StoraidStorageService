73332d016f96c748c56937d4ffcca41f8df86cfb
Emil L <emillarsson99@gmail.com>
2025-11-05 14:23:05 +0100

    ReAdded className to input form div

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,10 +1,10 @@
-function InputForm({ Text, shownText, placeholder, name, labelclassName, inputclassName, inputName, labelName }) {
+function InputForm({ Text, shownText, placeholder, name, labelclassName, inputclassName, inputName, labelName, className }) {
   return (
 
-    <div className="input-form">
+    <div className={`input-form ${className}`}>
 
       <form action="">
-        <label className={`label ${labelclassName}`}  htmlFor={Text}>{shownText} name={labelName}</label>
+        <label className={`label ${labelclassName}`}  htmlFor={Text} name={labelName} > {shownText} </label>
         <div className="form-row">
           <input className={`input ${inputclassName}`} type="text" placeholder={placeholder} name={inputName} />
         </div>
