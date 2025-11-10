86fd55236d0a05aa473b3bce885039bfeea0b360
Emil L <emillarsson99@gmail.com>
2025-11-10 09:19:56 +0100

    Added redirect to wrong element, problem is now fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,11 +11,11 @@ function Hero() {
                     <h1>Space Simplified, Storage Perfected</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor.</p>
                     <div className="hero-button">
-                        <Button className="hero-btn" Text="Discover more" />
+                        <Button className="hero-btn" Text="Discover more" to="/NotFoundPage" />
                     </div>
                 </div>
                 <div className="hero-image">
-                    <img src="/HeroFigure.svg" alt="Hero Figure" to="/NotFoundPage" />
+                    <img src="/HeroFigure.svg" alt="Hero Figure"/>
                 </div>
             </div>
         </div>
