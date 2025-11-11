2c0176db93231e9f8e1da7fd8f53c3d6ba696656
Emil L <emillarsson99@gmail.com>
2025-11-11 16:11:15 +0100

    Added ContactForm to ContactPage

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TitleCard from '../Components/TitleCard'
+import ContactForm from '../Components/ContactForm'
 
 function ContactPage() {
   return (
     <div>
       <TitleCard Title="Contact Us" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+      <ContactForm />
     </div>
   )
 }
