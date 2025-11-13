a9652a915501cd45b57ef4d74fe21bacd86106bb
Emil L <emillarsson99@gmail.com>
2025-11-13 10:08:49 +0100

    Added correct components to booking page. Moving to create bookingInputForm

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import TitleCard from '../Components/TitleCard'
+import WhyChooseUs from '../Components/WhyChooseUs'
+import Booking from '../Components/Booking'
+import FAQ from '../Components/FAQ'
 function BookingPage() {
   return (
-    <div>BookingPage</div>
+    <div>
+      <TitleCard Title="Booking" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+      <WhyChooseUs />
+      <Booking />
+      <FAQ />
+    </div>
   )
 }
 
